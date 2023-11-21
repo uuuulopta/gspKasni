877c59e3560e498b77acb07931082d29dc47b8e0
Ivan <uuuulopta@gmail.com>
2023-11-21 22:58:02 +0100

    Created busTableGetter and added json serialization

--- BusTable.cs
+++ BusTable.cs
@@ -1,4 +1,14 @@
-﻿namespace gspApiGetter7;
+﻿namespace gspApiGetter;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public enum TableDay
+{
+    Workday = 0,
+    Saturday,
+    Sunday
+}
 
 public class BusTable
 {
@@ -15,6 +25,38 @@ public class BusTable
         { TableDay.Sunday, new Dictionary<int, List<int>>() }
     };
 
+
+    public void saveAsJson(string filePath = "")
+    {
+        try
+        {
+            var contentsToWriteToFile = JsonConvert.SerializeObject(this);
+            var writer = new StreamWriter(filePath);
+            writer.Write(contentsToWriteToFile);
+            writer.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error has occured during file creation!");
+            Console.WriteLine(ex);
+        }
+
+    }
+
+    public static BusTable[] convertFromJsonString(string jsonString)
+    {
+        return JsonConvert.DeserializeObject<BusTable[]>(jsonString)!;
+
+    }
+
+    public static BusTable[] convertFromJsonFile(string filePath)
+    {
+        var reader = new StreamReader(filePath);
+        var fileContents = reader.ReadToEnd();
+        return JsonConvert.DeserializeObject<BusTable[]>(fileContents)!;
+    }
+
+
     public void writeOut()
     {
         int[] days = { (int)TableDay.Workday, (int)TableDay.Saturday, (int)TableDay.Sunday };
@@ -24,9 +66,12 @@ public class BusTable
         for (var i = 5; i <= 23; i++)
             try
             {
-                Console.Write($"Hour {i}: Workday:" + string.Join(", ", TableData[TableDay.Workday][i]));
-                Console.Write("\t"); Console.Write("Saturady:" + string.Join(", ",
-                    TableData[TableDay.Saturday][i])); Console.Write("\t");
+                Console.Write($"Hour {i}: Workday:" + string.Join(", ",
+                    TableData[TableDay.Workday][i]));
+                Console.Write("\t");
+                Console.Write("Saturday:" + string.Join(", ",
+                    TableData[TableDay.Saturday][i]));
+                Console.Write("\t");
                 Console.Write("Sunday:" + string.Join(", ",
                     TableData[TableDay.Sunday][i]));
                 Console.Write("\n");
---
+++ BusTableGetter.cs
@@ -0,0 +1,107 @@
+﻿namespace gspApiGetter;
+
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+public class BusTableGetter
+{
+    readonly HttpClient _client = new();
+    readonly double _cachedTimeOut;
+
+    public BusTableGetter(HttpClient? client = null, double cachedTimeOut = 12)
+    {
+        if (client == null)
+        {
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("text/html"));
+            _cachedTimeOut = 12;
+        }
+        else _client = client;
+    }
+
+    public BusTable[] getBusTablesFromHTML(string htmlString)
+    {
+        var html = new HtmlDocument();
+        html.LoadHtml(htmlString);
+        var tables = html.DocumentNode.SelectNodes("//table");
+        // t [direction][workday/saturday/sunday][hour] -> [minutes]
+        BusTable[] busTables = { new(), new() };
+        var tableCounter = 0;
+        foreach (var table in tables)
+        {
+            var hourCounter = 5;
+            var tbody = table.SelectNodes(".//tbody[1]//tr");
+            tbody.RemoveAt(tbody.Count - 1);
+            foreach (var tr in tbody)
+            {
+                var nodes = tr.ChildNodes.Where(node => node.NodeType != HtmlNodeType.Text).ToList();
+                for (var i = 0; i < 3; i++)
+                {
+                    var text = nodes[i + 1].InnerText;
+                    var minutes = new List<int>();
+                    if (text.Any(char.IsDigit))
+                    {
+                        var regex = new Regex(@"\b\d+\b");
+                        foreach (Match match in regex.Matches(text))
+                            minutes.Add(int.Parse(match.Value));
+
+                        busTables[tableCounter].TableData[(TableDay)i].Add(hourCounter,
+                            minutes);
+                    }
+                }
+
+                hourCounter++;
+            }
+
+            tableCounter++;
+        }
+
+        return busTables;
+    }
+
+    public BusTable[] getBusTableFromFile(string path)
+    {
+        var htmlString = File.ReadAllText(path);
+        return getBusTablesFromHTML(htmlString);
+    }
+
+    public BusTable[] getBusTableFromWeb(string id, bool checkCache = true, bool doCache = true)
+    {
+        string path = $"{id}.BusTable.json";
+        BusTable[] bust = { };
+        if (checkCache && File.Exists(path))
+        {
+            if (DateTime.Now.Subtract(File.GetLastWriteTime(path)).TotalHours > _cachedTimeOut)
+            {
+                File.Delete(path);
+                getBusTableFromWeb(id,
+                    checkCache,
+                    doCache);
+            }
+            else
+            {
+                bust = BusTable.convertFromJsonFile(path);
+            }
+
+        }
+        else
+        {
+            var htmlString = Task.Run(async () => await _getStationTable(id)).Result;
+            bust = getBusTablesFromHTML(htmlString);
+        }
+
+        return bust;
+
+    }
+
+    async Task<string> _getStationTable(string id)
+    {
+        // TODO: add progress output
+        Console.WriteLine($"Getting bus table for {id}...");
+        var res = await _client.GetStringAsync($"https://www.bgprevoz.rs/linije/red-voznje/linija/{id}/prikaz");
+        Console.WriteLine($"Got bus table {id}!");
+        return res;
+    }
+}
