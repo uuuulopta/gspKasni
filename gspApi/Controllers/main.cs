1d7f79a35d334a3f75053d5dcada4c35645d91b9
Ivan <uuuulopta@gmail.com>
2023-12-30 17:10:36 +0100

    Removed unused routes

--- gspApi/Controllers/main.cs
+++ gspApi/Controllers/main.cs
@@ -1,14 +1,10 @@
 ﻿namespace gspAPI.Controllers;
 
-using BusTableAPI;
 using DbContexts;
-using Entities;
 using gspApiGetter.BusTableAPI;
-using Mappings;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Services;
-using StackExchange.Profiling;
 
 [ApiController]
 [Route("BusTable")]
@@ -25,18 +21,6 @@ public class mainController : ControllerBase
         _busTableGetter = busTableGetter;
         _context = context;
     }
-    [HttpGet("/BusTable/{name}")]
-    public async Task<ActionResult<ICollection<BusTableDto>>> getTest(string name)
-    {
-        // var dtos = await _busTableGetter.getBusTableFromWebAndCache(name);
-       // var dtos = BusTableMapping.toDto(await _busTableRepository.getBusTablesByTime(8, 59, 1));
-       var busTables = await _busTableRepository.getBusTablesByName(name);
-       if (!busTables.Any()) return NotFound();
-       var dtos = Mappings.BusTableMapping.toDto(busTables);
-        
-        return Ok(dtos);
-    }
-
     [HttpGet("/pings")]
     public async Task<ActionResult<IEnumerable<PingData>>> getFormattedPings(int? from,int? to)
     {
@@ -51,19 +35,4 @@ public class mainController : ControllerBase
             if (res == null) return NotFound();
             return Ok(res);
         }
-    [HttpGet("/updateTable/{name}")]
-    public async Task<ActionResult> updateTable(string name)
-    {
-        _busTableRepository.deleteBusTablesByName(name);
-        await _busTableRepository.saveChangesAsync();
-        await _busTableGetter.getBusTableFromWebAndCache(name);
-        return Ok();
-    }
-
-
-    [HttpPost("/updateTables")]
-    public async Task getUpdates()
-    {
-        await _busTableGetter.updateAllTables();
-    }
 }
