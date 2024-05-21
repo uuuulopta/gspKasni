b2798fb4d08ee7a58a699aae4b74abb6a9af2458
Ivan <uuuulopta@gmail.com>
2024-05-21 19:01:34 +0200

    Fix typo bug

--- gspApi/Services/BusTableRepository.cs
+++ gspApi/Services/BusTableRepository.cs
@@ -208,7 +208,7 @@ public class BusTableRepository : IBusTableRepository
     public async Task<List<PingData>> getPingCacheFormattedDataFromDaily(int? from, int? to)
     {
         var fromDate = from.HasValue? DateTime.ParseExact(from.ToString()!, "yyyyMMdd",System.Globalization.CultureInfo.InvariantCulture) : DateTime.MinValue; 
-        var toDate = to.HasValue? DateTime.ParseExact(to.ToString()!, "yyyyMMdd",System.Globalization.CultureInfo.InvariantCulture) : DateTime.MinValue;
+        var toDate = to.HasValue? DateTime.ParseExact(to.ToString()!, "yyyyMMdd",System.Globalization.CultureInfo.InvariantCulture) : DateTime.MaxValue;
 
         IQueryable<DailyPingData> query = _context.DailyPingData;
         return await _context.DailyPingData.Include(b => b.BusRoute).GroupBy(dp => dp.BusRoute).Select(g => new PingData
