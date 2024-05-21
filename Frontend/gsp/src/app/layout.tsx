bf55af800c873c74c8ca08fd25894113eb398a36
Ivan <uuuulopta@gmail.com>
2024-05-21 18:29:11 +0200

    Added plausible support

--- Frontend/gsp/src/app/layout.tsx
+++ Frontend/gsp/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/themeprovider"
 import './globals.css'
+import PlausibleProvider from 'next-plausible'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="sr">
-
+      
+      <head> 
+        {process.env.PLAUSIBLE_DOMAIN_MONITOR ? <PlausibleProvider domain={process.env.PLAUSIBLE_DOMAIN_MONITOR}
+          customDomain={process.env.PLAUSIBLE_CUSTOM_DOMAIN ?? "https://plausible.io"}/> : null}
+      </head>
       <body className={inter.className}>
       <ThemeProvider
       attribute="class"
